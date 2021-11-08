b259b049cd472b78951ee2914248255a10978ffd
Lucas Santos <ceo.lucascs@gmail.com>
2021-11-08 20:29:59 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,57 @@
 'use strict';
 
-const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
+const browsersync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
-const webserver = require('gulp-webserver');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require("gulp-rename");
 const uglify = require('gulp-uglify');
 
-gulp.task('sass', function() {
-    return gulp.src('assets/css/sass/*.scss')
+function custom_browsersync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    done();
+}
+
+function custom_browsersync_reload(done) {
+    browsersync.reload();
+    done();
+}
+
+function custom_sass() {
+    return src('assets/css/sass/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(cssnano())
         .pipe(sourcemaps.write('./maps'))
         .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest('assets/css/min'));
-});
+        .pipe(dest('assets/css/min'));
+}
 
-gulp.task('js', function() {
-    return gulp.src('assets/js/*.js')
+function custom_js() {
+    return src('assets/js/*.js')
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write('./maps'))
         .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest('assets/js/min'));
-});
+        .pipe(dest('assets/js/min'));
+}
 
-gulp.task('default', function() {
-    gulp.src('/')
-        .pipe(webserver({
-            livereload: true,
-            directoryListing: true,
-            open: true
-        }));
-    gulp.watch('assets/css/sass/*.scss', gulp.series('sass'));
-    gulp.watch('assets/js/*.js', gulp.series('js'));
-});
+function custom_watch() {
+    watch('*.html', custom_browsersync_reload);
+    watch('assets/css/sass/*.scss', series(custom_sass, custom_browsersync_reload));
+    watch('assets/js/*.js', series(custom_js, custom_browsersync_reload));
+}
+
+exports.default = series(
+    custom_browsersync,
+    custom_sass,
+    custom_js,
+    custom_watch
+);
